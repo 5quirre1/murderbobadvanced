4349204f3528d41b79e63ff68c8f94a49a094ccf
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-09 18:54:13 -0600

    add cutscene 3

--- Code/Python/Game.py
+++ Code/Python/Game.py
@@ -87,5 +87,19 @@ def cutscene2():
         else:
             break
     cap.release()
+    cutscene3()
+def cutscene3():
+    cap = cv2.VideoCapture('cutscenes/cutscene3.mp4')
+    if (cap.isOpened()== False):
+        print("Error opening video file")
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imshow('MurderbobADVANCED | Cutscene 2', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
     cv2.destroyAllWindows()
 gameUI()
